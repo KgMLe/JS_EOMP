a138872bb7fb27dbc3b9e119ed323b02d64d403c
Kgodisho <kgodisholebopa@gmail.com>
2023-06-12 23:45:41 +0200

    Push5

--- code/checkout.js
+++ code/checkout.js
@@ -1,2 +1,20 @@
-let checkout = JSON.parse(localStorage.getItem('checkout')) ? 
-JSON.parse(localStorage.getItem('checkout')) : [];
+let checkout = JSON.parse(localStorage.getItem('prodList')) ? 
+JSON.parse(localStorage.getItem('prodList')) : [];
+
+let cartItem = document.querySelector('.checkOut');
+
+products.forEach((data)=> {
+  cartItem.innerHTML += `
+  <tr>
+  <th scope="row">${data.id}</th>
+  <td>${data.brand}</td>
+  <td>${data.src}</td>
+  <td>${data.description}</td>
+  <td>${data.price}</td>
+  <tr>
+  `
+
+})
+
+
+
